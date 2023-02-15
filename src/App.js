a07b6e5783f151eeb95a0f344fd91d029f9ac0e9
Adam Baldwin <adi.baldwin@hotmail.com>
2023-02-15 14:25:09 +0000

    cats fetched and displayed, temp resized

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 
 import './App.css';
-import {useState} from 'react';
-
-
+import {useEffect, useState} from 'react';
 
 const App = () => {
-  const [Cat, SetCat] = useState([]);
+  const [CatPics, SetCatPics] = useState([]);
+  const [ErrorMsg, SetErrorMsg] = useState(null);
+
+  useEffect(() => {
   const fetchCat = async () => {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10')
-  }    
+    try {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
+    if (!response.ok){
+      throw new Error(response.statusText)
+    }
+    const CatPicData = await response.json();
+    SetCatPics(CatPicData)
+  } catch (err) {
+    SetErrorMsg("Oops, a cat has knocked the router off the shelf. Please try again")
+  }
+  };
+
+  fetchCat()
+
+}, []);
+
+  return (
+    <>
+    <h1>Cats for Life Page header</h1>
+    {ErrorMsg !== null && <h3>{ErrorMsg}</h3>}
+    {CatPics.map((CatPicData, index) =>{
+      return (
+      <div className='AllCats'>
+        <img className='indivCats' key={index} src={CatPicData.url} alt="cat"></img> 
+        <p >Cat name will go here?</p>
+      </div>
+      )
+      })}
+      <div className='Basket'>
+        <h1>The Basket will go here?</h1>
+      </div>
+    </>
+  )  
 }
 
 export default App;
